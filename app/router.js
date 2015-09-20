1d3266f78d7a63fcc88e71da6c4f08011ba1ab5e
Chris Caragianis <chriscaragianis@gmail.com>
2015-09-19 20:36:13 -0400

    Ember app functioning

--- app/router.js
+++ app/router.js
@@ -6,6 +6,12 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.resource('kids');
+  this.resource('borrowing', function() {
+    this.resource('circ');
+    this.resource('check');
+    this.resource('placing');
+  });
 });
 
 export default Router;
