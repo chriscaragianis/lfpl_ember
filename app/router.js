4b36db2bce5ad4a8be275d10d81d6c64469e4479
Chris Caragianis <chriscaragianis@gmail.com>
2015-09-21 13:33:03 -0400

    Continue to add content and routes to said content

--- app/router.js
+++ app/router.js
@@ -7,10 +7,23 @@ var Router = Ember.Router.extend({
 
 Router.map(function() {
   this.resource('kids');
+  this.resource('research', function() {
+    this.resource('ill', {path: '/research/ill'});
+    this.resource('internet');
+    this.resource('geneaology');
+    this.resource('government');
+    this.resource('newspapers');
+  });
+  this.resource('learn-online');
+  this.resource('locations');
+  this.resource('events');
+  this.resource('facilities');
+  this.resource('services');
   this.resource('borrowing', function() {
     this.resource('circ');
     this.resource('check');
     this.resource('placing');
+    this.resource('card', {path: '/borrowing/card'});
   });
 });
 
